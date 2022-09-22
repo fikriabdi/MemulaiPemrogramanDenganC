a8ccce323374ca5b5222744627e864a2708b4e46
Fikri Abdi Putra <93390553+fikriabdi@users.noreply.github.com>
2022-09-22 15:07:16 +0700

    menulis program halo dunia dengan return
    menulis program halo dunia yang tidak hanya menuliskan sebuah string (deretan huruf), namun juga mengirimkan atau mengembalikan ke Anda (return) sebuah nilai 0 yang dinyatakan dalam perintah return 0

---
+++ halo1.c
@@ -0,0 +1,12 @@
+/* File : halo1.c */
+/* Penulis : Fikri Abdi Putra, email: fikriabdiputra21@gmail.com */
+/* Program untuk menuliskan Halo Dunia! ke layar */
+#include <stdio.h>
+
+int main()
+{ /* KAMUS */
+
+/* ALGORITMA */
+  printf("Halo Dunia!\n");
+  return 0;
+}
