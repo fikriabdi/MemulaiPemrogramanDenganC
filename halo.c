bbb7f706cf905cabccad7f1608fa9171feb87bac
Fikri Abdi Putra <93390553+fikriabdi@users.noreply.github.com>
2022-09-22 14:56:59 +0700

    menulis program halo dunia
    menulis program sederhana "Halo Dunia!" sebanyak 3 kali dimana, program ini tanpa mengembalikan nilai. Program ini tidak mengirimkan nilai apapun, hanya menuliskan string.

---
+++ halo.c
@@ -0,0 +1,11 @@
+/* File : halo.c */
+/* Penulis : Fikri Abdi Putra, email: fikriabdiputra21@gmail.com */
+/* Program menuliskan "Halo Dunia! "  sebanyak 3 kali dan "end-of-line" (ganti baris) */
+#include <stdio.h>
+
+int main(void) 
+{ /* Badan Program untuk menuliskan "Halo Dunia!" 3 kali */
+  printf("Halo Dunia!\n");
+  printf("Halo Dunia!\n");
+  printf("Halo Dunia!\n");
+}
