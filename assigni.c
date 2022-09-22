f62235bdb565cca712aae6261a0387bfd2ad3de3
Fikri Abdi Putra <93390553+fikriabdi@users.noreply.github.com>
2022-09-22 16:00:14 +0700

    input variabel i dengan assignment '='
        Memahami makna dan dampak “=” dalam bahasa C,  dalam sebuah assignment.
        Memahami dampak eksekusi “%d” dan hubungannya dengan nilai variabel, pada perintah printf dalam bahasa C.
        Membedakan perbedaan makna dari “=” pada suatu deklarasi, dibandingkan dengan instruksi “assignment.”
        Memahami bahwa hasil komputasi di ruas kanan tanda “=” akan disimpan di variabel di ruas kiri, pada instruksi i = i + 1;.

---
+++ assigni.c
@@ -0,0 +1,18 @@
+/* File : assigni.c */
+/* Penulis : Fikri Abdi Putra, email: fikriabdiputra21@gmail.com */
+/* Mengisi variabel i yang ber-type in dengan nilai dan menuliskannya*/
+#include <stdio.h>
+
+int main()
+{ /* kamus */
+  int i;
+
+/* Algoritma */
+  i = 10; /* variabel i bernilai 10 */
+  printf("Ini nilai i : %d \n", i); 
+
+  i = i+5; /* nilai i ditambah 5, hasilnya disimpan kembali ke i */
+  printf("Ini nilai i : %d \n", i);
+  
+  return 0;
+}
