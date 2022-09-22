f2c2df934712cb38239735e9868b9936a5555cb3
Fikri Abdi Putra <93390553+fikriabdi@users.noreply.github.com>
2022-09-22 15:42:57 +0700

    Input nilai dengan int dan float
    memahami nama variabel dan pengisian variabel int dan float dan memahami perintah eksekusi %d dan %f.

---
+++ initvar.c
@@ -0,0 +1,20 @@
+/* File : initvar.c */
+/* Penulis : Fikri Abdi Putra, email: fikriabdiputra21@gmail.com */
+/* Mengisi variabel i yang bertype in dengan nilai */
+#include <stdio.h>
+
+int main()
+{ /* kamus */
+  int bebek = 10; /* deklarasi dan inisialisasi nilai variabel bebek dengan nilai 10 */
+  float luas = 2.5; /* nilai luas kebun sebesar 2.5 hektar*/
+  float keliling = 2*3.14*5.5; /* nilai keliling lingkaran dengan jari-jari 5.5*/
+
+/* Algoritma */
+  printf("Ini jumlah bebekku : %d \n", bebek); /*untuk mengamati    dampak inisialisasi*/
+  printf("kebunku di desa seluas : %f \n", luas);
+  printf("jumlah nilai keliling lingkaran sebesar: %f \n",     keliling);
+  return 0;
+}
+
+
+/* untuk eksekusi variabel int menggunakan %d, sedangkan untuk eksekusi variabel float menggunakan %f */
